4bfcb4df88c9f0a291a603700db84f76b569d02c
Prakash Dhankecha <pdhankecha232@rku.ac.in>
2023-03-24 18:53:00 +0530

    Revert "auth controller, autho route, partially done"
    This reverts commit 1941c230bc2173145e3eec6083ca17d73d8dab6a.

---
+++ controllers/reviewController.js
@@ -0,0 +1,24 @@
+import Bus from '../models/Bus.js'
+import Review from  '../models/Review.js'
+
+export const createReview = async (req, res) => {
+
+    const busId = req.params.busId
+    const newReview = new Review({...req.body})
+
+    try {
+        const savedReview = await newReview.save()
+
+        // after creating new review now update the reviews array of the bus
+        await Bus.findByIdandUpdate(busId,{
+            $push: {
+                reviews: savedReview._id
+            }
+        })
+
+        res.status(200).json({success: true, message:"Reviews submitted", data:savedReview})
+
+    } catch (error) {
+        res.status(500).json({success: false, message:"failed to submit"})
+    }
+}
