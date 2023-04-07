5b80e101d91ca28af0c06dcec478e8caa9b18f65
Prakash Dhankecha <pdhankecha232@rku.ac.in>
2023-04-07 20:56:25 +0530

    .

--- routes/booking.js
+++ routes/booking.js
@@ -1,12 +1,15 @@
-import express from "express"
-import { createBooking, getAllBooking, getBooking } from "../controllers/bookingController.js"
-import { verifyAdmin, verifyUser } from "../utils/verifyToken.js"
+import express from "express";
+import {
+  createBooking,
+  getAllBooking,
+  getBooking,
+} from "../controllers/bookingController.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
+const router = express.Router();
 
-const router = express.Router()
+router.post("/", verifyUser, createBooking);
+router.get("/:id", verifyUser, getBooking);
+router.get("/", verifyAdmin, getAllBooking);
 
-router.post('/', verifyUser, createBooking)
-router.get('/:id', verifyUser, getBooking)
-router.get('/', verifyAdmin, getAllBooking)
-
-export default router
+export default router;
